d2dd1ba9097a98f341838218e376e6a845c00b48
Ian Derrington <ian.derrington@gmail.com>
2026-02-22 21:45:00 -0800

    fix: add React import to PostHogProvider

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React from 'react';
+
 import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { useEffect } from 'react';
